39fb290fec16681dab7cdc6c7b0f7ed5dde2ba3a
Valeriy Evdokimov <valeriy.evdokimov@gmail.com>
2017-09-20 11:04:45 +0300

    Модуль 6, задание 1, начало
    Реализована сортировка и обработчики событий для фильтров.
    Реализован постепенный постраничный вывод (без таймаута).

--- src/pictures.js
+++ src/pictures.js
@@ -14,19 +14,78 @@
   var DATA_LOAD_URL = '/json/pictures.json';
 
   /**
-   * Массив данных о плитках для фона
+  * Размеры плитки
+  * @const
+  * @type {Number}
+  */
+  var TILE_WIDTH = 196;
+  var TILE_HEIGHT = 196;
+
+  /**
+   * Константа для перерисовки экрана
+   * @const
+   * @type {Boolean}
+   */
+  var REDRAW_PAGE = true;
+
+  /**
+   * Типы сортировки
+   * @enum {number}
+   */
+  var SortType = {
+    POPULAR: 'popular',
+    NEW: 'new',
+    DISCUSSED: 'discussed'
+  };
+
+  /**
+   * Тип сортировки по-умолчанию
+   * @const
+   * @type {SortType}
+   */
+  var DEFAULT_SORT_TYPE = SortType.POPULAR;
+
+  /**
+  * Тип сортировки
+  * @type {Object}
+  */
+  var sortType = {
+    type: DEFAULT_SORT_TYPE,
+    ascending: false
+  };
+
+  /**
+   * Исходный массив данных о плитках
    * @type {Array.<Object>}
    */
   var tilesArray = [];
 
   /**
-   * Контейнер шаблонов элементов
+   * Массив данных о плитках для отрисовки
+   * @type {Array.<Object>}
+   */
+  var filteredTilesArray = [];
+
+  /**
+   * Счетчик отрисованных страниц
+   * @type {Number}
+   */
+  var pagesCount;
+
+  /**
+   * Счетчик отрисованных плиток
+   * @type {Number}
+   */
+  var tilesCount;
+
+  /**
+   * Шаблон плитки
    * @type {HTMLElement}
    */
   var tileTemplate;
 
   /**
-   * Шаблон плитки
+   * Контейнер шаблонов
    * @type {HTMLElement}
    */
   var templateElement = document.querySelector('#picture-template');
@@ -105,69 +164,178 @@
   };
 
   /**
+   * Рассчет количества плиток на одну страницу (с учетом первой страницы)
+   * @param {boolean} isFirstPage
+   * @returns {number}
+   */
+  var getTilesPerPage = function(page) {
+    // главная форма приложения Кекстаграм
+    var uploadForm = document.forms['upload-select-image'];
+
+    // Ширина страницы равна ширине контейнеа, а вот высота - от высоты окна.
+    // Для простоты рассчетов сразу округляем до величины, кратной размеру плитки
+    var pageWidth = Math.floor(tilesContainer.clientWidth / TILE_WIDTH) * TILE_WIDTH;
+    var pageHeight = Math.ceil(document.documentElement.clientHeight / TILE_HEIGHT) * TILE_HEIGHT;
+
+    // рассчитываем видимую высоту формы приложения на заданной странице
+    var uploadFormHeight = Math.max(Math.min(uploadForm.clientHeight - pageHeight * page, pageHeight), 0);
+
+    // Рассчитываем, сколько плиток помещается по ширине и высоте
+    var hCount = pageWidth / TILE_WIDTH;
+    var vCount = pageHeight / TILE_HEIGHT;
+
+    // Рассчитываем, сколько плиток занимает видимая часть главной формы приложения
+    var uploadFormSize = Math.ceil(uploadFormHeight / TILE_HEIGHT) * Math.ceil(uploadForm.clientWidth / TILE_WIDTH);
+
+    return hCount * vCount - uploadFormSize;
+  };
+
+  /**
+   * Проверка, приблизился ли низ контейнера к низу экрана на величину половины плитки
+   * @returns {boolean}
+   */
+  var isBottomReached = function() {
+    var gap = TILE_HEIGHT / 2;
+    var containerBottom = tilesContainer.getBoundingClientRect().bottom;
+    var windowHeight = document.documentElement.clientHeight;
+    return containerBottom < (windowHeight + gap);
+  };
+
+  /**
+   * Проверка, отрисован ли весь массив плиток
+   * @returns {boolean}
+   */
+  var isArrayDrawn = function() {
+    return tilesCount >= filteredTilesArray.length;
+  };
+
+  /**
    * Отрисовывает коллекцию плиток в контейнер
    * @param {Array.<Object>} arr
+   * @param {boolean} clear
    */
-  var renderTilesCollection = function(arr) {
-    // Проверка, что фильтры спрятаны, иначе прячем
-    if (!tileFilterForm.classList.contains('hidden')) {
-      tileFilterForm.classList.add('hidden');
+  var renderTilesCollection = function(arr, clear) {
+    // Если флаг офистки выставлен в true, то очищаем контейнер и обнуляем счетчики
+    if (clear) {
+      tilesContainer.innerHTML = '';
+      pagesCount = 0;
+      tilesCount = 0;
     }
 
-    // очистка контейнера
-    tilesContainer.innerHTML = '';
-
-    arr.forEach(function(tile) {
+    // Копируем часть массива для отрисовки, начиная с последнего отрисованного в контейнере элемента в расчитанном количестве
+    var arrToRender = arr.slice(tilesCount, tilesCount + getTilesPerPage(pagesCount));
+    arrToRender.forEach(function(tile) {
       // Добавление полностью настроенного клонрованного элемента в контейнер
       tilesContainer.appendChild(getTileElement(tile));
+      // Увеличиваем счетчик отрисованных элементов
+      tilesCount++;
     });
 
-    // Показываем фильтры только когда все плитки отрисованы
-    tileFilterForm.classList.remove('hidden');
+    // Увеличиваем счетчик отрисованных страниц
+    pagesCount++;
   };
 
   /**
    * Сортировка массива плиток
    * @param {string} sort
-   * @returns {Array.<Object>}
+   * @param {Boolean} ascending
    */
-  var sortTileArray = function(sort) {
-    var arr = tilesArray.slice(0);
+  var sortTileArray = function(sort, ascending) {
 
-    switch (sort) {
-      case 'popular':
-        // возвращаем исодный массив
-        arr = tilesArray;
-        break;
-      case 'new':
-        arr = arr.sort(function(a, b) {
-          // сортировка даты по убыванию
-          return new Date(b.date) - new Date(a.date);
-        }).filter(function(tile) {
+    /**
+     * Сортировка массива, возвращает новый массив
+     * @param {Array.<Object>} arr
+     * @returns {Array.<Object>}
+     */
+    var sortArray = function(arr) {
+      /** @type {string} атрибут, по которому сортируем */
+      var atr;
+
+      /** @type {Object.<number, number>[]} мапированный массив с нормализованными данными для сортировки */
+      var indexArr = [];
+
+      /** @type {number} 1 или -1, множитель для задания направления сортировки */
+      var order = (-!ascending || +ascending);
+
+      // В зависимости от значения sort, выбираем атрибут, по которому будем вести сортировку
+      switch (sort) {
+        case SortType.NEW:
+          atr = 'date';
+          break;
+        case SortType.DISCUSSED:
+          atr = 'comments';
+          break;
+        default:
+        case SortType.POPULAR:
+          atr = 'likes';
+          break;
+      }
+
+      // Мапируем массив значений, по которым будет осуществлятьс сортировка
+      // Данная теника позволяет избежать манипуляций с большим массивом сложных объектов
+      if (sort === SortType.NEW) {
+        indexArr = arr.map(function(element, i) {
+          return {'value': Date.parse(element[atr]), 'index': i};
+        });
+      } else {
+        indexArr = arr.map(function(element, i) {
+          return {'value': element[atr], 'index': i};
+        });
+      }
+
+      indexArr.sort(function(a, b) {
+        // Выполняем сортиовку массива значений, выбирая направление в зависимости от order
+        return (a.value - b.value) * order;
+      });
+
+      // возвращаем новый массив объектов, в соответствии с отсортированным масивом значений
+      return indexArr.map(function(element) {
+        return arr[element.index];
+      });
+    };
+
+    /**
+     * Фильтрация массива
+     * @param {Array.<Object>} arr
+     * @returns {Array.<Object>}
+     */
+    var filterArray = function(arr) {
+      var resultArray = arr.slice(0);
+
+      // Выполняем фильтрацию исходного массива, если требуется, и возвращаем новый массив
+      if (sort === SortType.NEW) {
+        return resultArray.filter(function(element) {
           // филтрация по датам за последние 2 недели
-          // return new Date(tile.date) > (Date.now() - 14 * 24 * 60 * 60 * 1000);
-          // поскольку данных за 2 недели мало, для красоты возвращает тру всегда))
-          return tile;
-        });
-        break;
-      case 'discussed':
-        arr.sort(function(a, b) {
-          // сортировка каментов по убыванию
-          return b.comments - a.comments;
-        });
-        break;
-    }
+          // return Date.parse(element.date) > (Date.now() - 14 * 24 * 60 * 60 * 1000);
 
-    return arr;
+          // поскольку данных за 2 недели мало, для красоты возвращает тру всегда))
+          return element;
+        });
+      }
+
+      return resultArray;
+    };
+
+    // фильтруем, а затем сортируем исходный массив, получая на выходе новый массив
+    filteredTilesArray = sortArray(filterArray(tilesArray));
+
+    // Отрисовываем отсортированный массив
+    renderTilesCollection(filteredTilesArray, REDRAW_PAGE);
   };
 
   /**
    * обработчик изменения выбранного фильтра
    * @param {Event} evt
    */
-  var tileFilterChange = function(evt) {
-    // отрисовываем осортированнй нужным образом массив
-    renderTilesCollection(sortTileArray(evt.target.value));
+  var tileFilterSet = function(evt) {
+    if (evt.target.tagName === 'INPUT' && evt.target.type === 'radio') {
+      // Если новая сортировка соответствует установленной, то сортируем в обратном порядке
+      sortType.ascending = (sortType.type === evt.target.value) && !sortType.ascending;
+      sortType.type = evt.target.value;
+
+      // сортируем и отрисовываем массив
+      sortTileArray(sortType.type, sortType.ascending);
+    }
   };
 
   /**
@@ -182,7 +350,7 @@
       tilesContainer.classList.add('pictures-loading');
     };
 
-    // обработка события ошибка или таймаут
+    // обработка события "ошибка" или "таймаут"
     var xhrOnError = function() {
       tilesContainer.classList.remove('pictures-loading');
       tilesContainer.classList.add('pictures-failure');
@@ -202,8 +370,13 @@
         // обрабатываем случай ошибки парсинга полученных данных
         try {
           tilesArray = JSON.parse(evt.target.response);
-          // отрисовываем полученные данные
-          renderTilesCollection(tilesArray);
+
+          //  устанавливаем филтрацию по умолчанию и отрисовываем полученные данные
+          tileFilterForm['filter'].value = DEFAULT_SORT_TYPE;
+          sortTileArray(DEFAULT_SORT_TYPE);
+
+          // Показываем фильтры только когда все плитки отрисованы
+          tileFilterForm.classList.remove('hidden');
 
           // удаление класса для отображения статуса загрузки с задержкой
           setTimeout(function() {
@@ -220,6 +393,11 @@
       // если исполнение дошло до сюда, то произошла ошибка
       xhrOnError();
     };
+
+    // Прячем фильтры, если они показаны
+    if (!tileFilterForm.classList.contains('hidden')) {
+      tileFilterForm.classList.add('hidden');
+    }
 
     // настраиваем запрос
     xhr.open('GET', url);
@@ -242,8 +420,32 @@
     tileTemplate = templateElement.querySelector('.picture');
   }
 
-  // устанавливаем обработчик для фильтров
-  tileFilterForm.onchange = tileFilterChange;
+  window.onresize1 = function() {
+    debugger;
+    // Перерисовываем плитки постранично до тех пор, пока не отрисуем столько же, либо больше
+    var clear = true;
+    var drawnTilesCount = tilesCount;
+    do {
+      renderTilesCollection(filteredTilesArray, clear);
+      clear = false;
+    } while (tilesCount < drawnTilesCount);
+  };
+
+  // обработчик события "прокрутка"
+  window.onscroll = function() {
+    // запускаем отрисовку массива, в случае, если низ страницы уже достигнут, а массив полностью ещё не отрисован
+    if (isBottomReached() && !isArrayDrawn()) {
+      renderTilesCollection(filteredTilesArray);
+    }
+  };
+
+  // устанавливаем обработчики событий для фильтров
+  tileFilterForm.onclick = tileFilterSet;
+  tileFilterForm.onkeydown = function(evt) {
+    if (evt.keyCode === 13 || evt.keyCode === 32) {
+      tileFilterSet(evt);
+    }
+  };
 
   // загрузка данных о плитках по HTTP
   loadTilesData(DATA_LOAD_URL);
